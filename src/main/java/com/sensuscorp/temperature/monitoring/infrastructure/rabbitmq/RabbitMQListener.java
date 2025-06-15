0c4bc869605c02bd44e67dbaac9c26967877c17f
savio <savio_ls@hotmail.com>
2025-06-15 13:52:25 -0300

    Múltiplas Queues para um consumidor

--- src/main/java/com/sensuscorp/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/sensuscorp/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
-import static com.sensuscorp.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE;
+import static com.sensuscorp.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.*;
 
 
 @Slf4j
@@ -21,10 +21,17 @@ public class RabbitMQListener {
 
     private final TemperatureMonitoringService temperatureMonitoringService;
 
-    @RabbitListener(queues = QUEUE, concurrency = "2-3")
+    @RabbitListener(queues = QUEUE_PROCESS_TEMPERATURE, concurrency = "2-3")
     @SneakyThrows
-    public void handle(@Payload TemperatureLogData temperatureLogData) {
+    public void handleProcessingTemperature(@Payload TemperatureLogData temperatureLogData) {
         temperatureMonitoringService.processTemperatureReading(temperatureLogData);
+        Thread.sleep(Duration.ofSeconds(5));
+    }
+
+    @RabbitListener(queues = QUEUE_ALERTING, concurrency = "2-3")
+    @SneakyThrows
+    public void handleAlerting(@Payload TemperatureLogData temperatureLogData) {
+        log.info("Alerting: SensorId {} Temp {}", temperatureLogData.getSensorId(), temperatureLogData.getValue());
         Thread.sleep(Duration.ofSeconds(5));
     }
 
