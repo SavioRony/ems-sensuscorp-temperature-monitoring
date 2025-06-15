d6dba79607bf228e393cc38c4467b7935f9f7338
savio <savio_ls@hotmail.com>
2025-06-15 13:51:37 -0300

    Consumer do RabbitMQ

---
+++ src/main/java/com/sensuscorp/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -0,0 +1,31 @@
+package com.sensuscorp.temperature.monitoring.infrastructure.rabbitmq;
+
+import com.sensuscorp.temperature.monitoring.api.model.TemperatureLogData;
+import com.sensuscorp.temperature.monitoring.domain.service.TemperatureMonitoringService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+import static com.sensuscorp.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMQListener {
+
+    private final TemperatureMonitoringService temperatureMonitoringService;
+
+    @RabbitListener(queues = QUEUE, concurrency = "2-3")
+    @SneakyThrows
+    public void handle(@Payload TemperatureLogData temperatureLogData) {
+        temperatureMonitoringService.processTemperatureReading(temperatureLogData);
+        Thread.sleep(Duration.ofSeconds(5));
+    }
+
+}
